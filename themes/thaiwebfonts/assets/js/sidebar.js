ad5e5e3a053abe5148c3b5a8eebff7ba8b9b8efb
LazyWasabi <12095896+lazywasabi@users.noreply.github.com>
2024-09-07 20:59:10 +0700

    ปรับปรุงสคริปต์สำหรับหน้าที่ไม่มี sidebar

--- themes/thaiwebfonts/assets/js/sidebar.js
+++ themes/thaiwebfonts/assets/js/sidebar.js
@@ -1,15 +1,16 @@
 // Scroll the active sidebar link into view
-const sidenav = document.querySelector('.twf-sidebar .offcanvas-body');
-const sidenavHeight = sidenav.clientHeight;
-const sidenavActiveLink = document.querySelector(
-  '.twf-sidebar .offcanvas-body a.active'
-);
-const sidenavActiveLinkTop = sidenavActiveLink.offsetTop;
-const sidenavActiveLinkHeight = sidenavActiveLink.clientHeight;
-const viewportTop = sidenavActiveLinkTop;
-const viewportBottom = viewportTop - sidenavHeight + sidenavActiveLinkHeight;
+const sidenavActiveLink = document.querySelector('.twf-sidebar .offcanvas-body a.active');
 
-if (sidenav.scrollTop > viewportTop || sidenav.scrollTop < viewportBottom) {
-  sidenav.scrollTop =
-    viewportTop - sidenavHeight / 2 + sidenavActiveLinkHeight / 2;
+if (sidenavActiveLink) {
+  const sidenav = document.querySelector('.twf-sidebar .offcanvas-body');
+  const sidenavHeight = sidenav.clientHeight;
+  const sidenavActiveLinkTop = sidenavActiveLink.offsetTop;
+  const sidenavActiveLinkHeight = sidenavActiveLink.clientHeight;
+  const viewportTop = sidenavActiveLinkTop;
+  const viewportBottom = viewportTop - sidenavHeight + sidenavActiveLinkHeight;
+
+  if (sidenav.scrollTop > viewportTop || sidenav.scrollTop < viewportBottom) {
+    sidenav.scrollTop =
+      viewportTop - sidenavHeight / 2 + sidenavActiveLinkHeight / 2;
+  }
 }
