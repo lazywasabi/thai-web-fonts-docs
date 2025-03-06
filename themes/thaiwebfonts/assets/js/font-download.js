6984cfabee50b61210259fc626fb53ab64d0b4f9
LazyWasabi <12095896+lazywasabi@users.noreply.github.com>
2025-03-06 21:50:49 +0700

    ปรับปรุงสคริปต์ดาวน์โหลดฟอนต์ สร้างและลบ iframe เมื่อเปิด-ปิด modal แทนการเปลี่ยน src แก้กดปุ่ม back ในเบราว์เซอร์ไม่ได้

--- themes/thaiwebfonts/assets/js/font-download.js
+++ themes/thaiwebfonts/assets/js/font-download.js
@@ -1,13 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const myModal = document.getElementById('downloadModal');
-  const downloadFrame = document.getElementById('downloadFrame');
+  const downloadModal = document.getElementById('downloadModal');
+  const downloadFrame = document.getElementById('download-frame-container');
 
-  myModal.addEventListener('shown.bs.modal', function (event) {
-    const iframeSrc = downloadFrame.dataset.src;
-    downloadFrame.src = iframeSrc;
-  });
+  if (downloadModal) {
+    downloadModal.addEventListener('shown.bs.modal', function (event) {
+      const iframeSrc = downloadFrame.dataset.src;
+      const iframe = document.createElement('iframe');
+      iframe.src = iframeSrc;
+      iframe.style.width = '100%';
+      iframe.style.height = '150px';
+      iframe.style.border = '0';
+      downloadFrame.appendChild(iframe);
+    });
 
-  myModal.addEventListener('hidden.bs.modal', function (event) {
-    downloadFrame.src = "";
-  });
+    downloadModal.addEventListener('hidden.bs.modal', function (event) {
+      downloadFrame.innerHTML = '';
+    });
+  }
 });
