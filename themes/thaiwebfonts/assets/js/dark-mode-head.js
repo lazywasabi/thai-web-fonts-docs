7cc6cb2fe092085bd805500d0ee699060ce0c635
LazyWasabi <12095896+lazywasabi@users.noreply.github.com>
2025-04-08 17:56:20 +0700

    ปรับปรุงสคริปต์ dark mode ย้ายบางส่วนไปฝัง inline ไว้บน head แก้การเปลี่ยนธีมเวลาเปลี่ยนหน้าเว็บ

---
+++ themes/thaiwebfonts/assets/js/dark-mode-head.js
@@ -0,0 +1,17 @@
+const getStoredTheme = () => localStorage.getItem('theme')
+const setStoredTheme = theme => localStorage.setItem('theme', theme)
+
+const getPreferredTheme = () => {
+  const storedTheme = getStoredTheme()
+  return storedTheme ? storedTheme : 'auto';
+}
+
+const setTheme = theme => {
+  if (theme === 'auto') {
+    document.documentElement.setAttribute('data-bs-theme', (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'))
+  } else {
+    document.documentElement.setAttribute('data-bs-theme', theme)
+  }
+}
+
+setTheme(getPreferredTheme())
