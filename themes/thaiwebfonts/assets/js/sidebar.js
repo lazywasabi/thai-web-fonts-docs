6983404d417f8a1e5023507a0e12ef1e007490e2
LazyWasabi <12095896+lazywasabi@users.noreply.github.com>
2025-04-09 08:45:50 +0700

    แก้ sidebar.js ไม่ทำงานหลังอัปเดต

--- themes/thaiwebfonts/assets/js/sidebar.js
+++ themes/thaiwebfonts/assets/js/sidebar.js
@@ -1,5 +1,5 @@
 // Scroll the active sidebar link into view
-const sidenavActiveLink = document.querySelector('.twf-sidebar .offcanvas-body a.active');
+const sidenavActiveLink = document.querySelector('.twf-sidebar .offcanvas-body a.nav-link.active');
 
 if (sidenavActiveLink) {
   const sidenav = document.querySelector('.twf-sidebar .offcanvas-body');
@@ -7,7 +7,7 @@ if (sidenavActiveLink) {
   const sidenavActiveLinkTop = sidenavActiveLink.offsetTop;
   const sidenavActiveLinkHeight = sidenavActiveLink.clientHeight;
   const viewportTop = sidenavActiveLinkTop;
-  const viewportBottom = viewportTop - sidenavHeight + sidenavActiveLinkHeight;
+  const viewportBottom = viewportTop - sidenavHeight + sidenavActiveLinkHeight + 300;
 
   if (sidenav.scrollTop > viewportTop || sidenav.scrollTop < viewportBottom) {
     sidenav.scrollTop =
